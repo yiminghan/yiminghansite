0dc06b1bc97233024c39ee8483d0aa463a944404
YiMing Han <yiming@nanoleaf.me>
2019-07-01 12:25:24 -0400

    update deployment stuff

--- var/app/current/server.js
+++ var/app/current/server.js
@@ -3,10 +3,12 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', 3030);  
+const PORT = process.env.PORT || 3030;
+
+app.set(PORT, 3030);  
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3030, function(err) {
+app.listen(PORT, function(err) {
   if (err) {
     return console.error(err);
   }
